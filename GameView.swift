a1637a3d817e87c9bf8c8b84d6cc3e011fb63b6d
Yongfeng <bravetsia@gmail.com>
2022-12-18 10:21:19 -0600

    Save results and display it.

--- GameView.swift
+++ GameView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 import ComposableArchitecture
 
+struct GameResult: Equatable {
+    let secret: Int
+    let guess: Int
+    let timeSpent: TimeInterval
+
+    var correct: Bool { secret == guess }
+}
+
 struct GameState: Equatable {
     var counter: CounterState = .init()
     var timer: TimerState = .init()
+    
+    var results: [GameResult] = []
+    var lastTimestamp = 0.0
 }
 
 enum GameAction {
@@ -20,7 +31,17 @@ enum GameAction {
 
 struct GameEnvironment { }
 
-let gameReducer = AnyReducer<GameState, GameAction, GameEnvironment>.combine(
+let gameReducer = AnyReducer<GameState, GameAction, GameEnvironment>.combine(.init { state, action, environment in
+    switch action {
+    case .counter(.reset):
+        let result = GameResult(secret: state.counter.secret, guess: state.counter.count, timeSpent: state.timer.duration - state.lastTimestamp)
+        state.results.append(result)
+        state.lastTimestamp = state.timer.duration
+        return .none
+    default:
+        return .none
+    }
+    },
     counterReducer.pullback(
         state: \.counter,
         action: /GameAction.counter,
@@ -36,13 +57,18 @@ let gameReducer = AnyReducer<GameState, GameAction, GameEnvironment>.combine(
 struct GameView: View {
     let store: Store<GameState, GameAction>
     var body: some View {
-        WithViewStore(store.stateless) { viewStore in
+        WithViewStore(store.scope(state: \.results)) { viewStore in
             VStack {
+                resultLabel(viewStore.state)
+                Divider()
                 TimerLabelView(store: store.scope(state: \.timer, action: GameAction.timer))
                 CounterView(store: store.scope(state: \.counter, action: GameAction.counter))
             }.onAppear {
                 viewStore.send(.timer(.start))
             }
         }
+    }
+    func resultLabel(_ results: [GameResult]) -> some View {
+        Text("Result: \(results.filter(\.correct).count)/\(results.count) correct")
     }
 }
